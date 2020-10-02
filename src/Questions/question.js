c3659e56cf830afd12f15fea8106a64a8d1b7c1c
otsang3 <ohhtsang@gmail.com>
2020-10-02 21:39:32 +0100

    added questions and answers file

---
+++ src/Questions/question.js
@@ -0,0 +1,171 @@
+export const questions = [
+    {
+        question: "During the end-of-year exams, you notice that one of your classmates was using an enchanted quill. You come top of the class anyway, but they are second. What do you do?",
+        answers: [
+            {
+                type: "Gryffindor",
+                content: "Tell the professor immediately – cheating is wrong, no matter what."
+            },
+            {
+                type: "Ravenclaw",
+                content: "Nothing, but if I hadn't come top of the class, I'd definitely tell the professor."
+            },
+            {
+                type: "Hufflepuff",
+                content: "Encourage the other student to admit what they'd done to the professor."
+            },
+            {
+                type: "Slytherin",
+                content: "Give them a high five for managing to sneak the quill into the exam."
+            }
+        ]
+    },
+    {
+        question: "You would be most hurt if a person called you...",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "Weak"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Ignorant"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Unkind"
+            },
+            {
+                type: "Gryffindor",
+                content: "Boring"
+            }
+        ]
+    },
+    {
+        question: "You're locked in a duel with a skilled opponent. They fire an unknown spell at you, and you shout…",
+        answers: [
+            {
+                type: "Gryffindor",
+                content: "Expelliarmus!"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Protego!"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Stupefy!"
+            },
+            {
+                type: "Slytherin",
+                content: "Crucio!"
+            }
+        ]
+    },
+    {
+        question: "Which of your skills are you most proud of?",
+        answers: [
+            {
+                type: "Ravenclaw",
+                content: "My ability to absorb new information."
+            },
+            {
+                type: "Hufflepuff",
+                content: "My ability to make new friends."
+            },
+            {
+                type: "Slytherin",
+                content: "My ability to get what I want."
+            },
+            {
+                type: "Gryffindor",
+                content: "My ability to keep secrets."
+            }
+        ]
+    },
+    {
+        question: "The first Quidditch match of the season is approaching, and you can't wait to get involved. What role are you playing?",
+        answers: [
+            {
+                type: "Ravenclaw",
+                content: "Seeker. I want the glory!"
+            },
+            {
+                type: "Gryffindor",
+                content: "Chaser. I like to be involved, and work as part of the team."
+            },
+            {
+                type: "Slytherin",
+                content: "Beater. I like having all that power."
+            },
+            {
+                type: "Hufflepuff",
+                content: "I'll be in the crowd, making sure supporter morale is high!"
+            }
+        ]
+    },
+    {
+        question: "It's Saturday, you've finished your homework, and you have some free time. You decide to spend some time away from your common room. Where do you go?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "The Forbidden Forest"
+            },
+            {
+                type: "Ravenclaw",
+                content: "The library"
+            },
+            {
+                type: "Hufflepuff",
+                content: "The kitchens"
+            },
+            {
+                type: "Gryffindor",
+                content: "The Room of Requirement"
+            }
+        ]
+    },
+    {
+        question: "What would you see in the Mirror of Erised?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "Myself, surrounded by riches"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Myself, surrounded by my loving family and friends"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Myself, knowledgable above all"
+            },
+            {
+                type: "Gryffindor",
+                content: "Myself, experiencing a marvellous adventure"
+            }
+        ]
+    },
+    {
+        question: "Which animal would be your mascot?",
+        answers: [
+            {
+                type: "Slytherin",
+                content: "Serpent"
+            },
+            {
+                type: "Hufflepuff",
+                content: "Badger"
+            },
+            {
+                type: "Ravenclaw",
+                content: "Eagle"
+            },
+            {
+                type: "Gryffindor",
+                content: "Lion"
+            }
+        ]
+    }
+  ];
+  

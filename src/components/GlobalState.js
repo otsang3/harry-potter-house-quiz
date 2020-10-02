f363c544f0eb58e3d97d356da0950f4a0552accb
otsang3 <ohhtsang@gmail.com>
2020-10-02 23:17:14 +0100

    added method in globalState to shuffle question answers, initialised state

--- src/components/GlobalState.js
+++ src/components/GlobalState.js
@@ -1,13 +1,32 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { questions } from '../questions/Questions';
 
 export const GlobalContext = createContext();
 
 function GlobalState(props) {
 
-    const obj = {test:"test"}
+    useEffect(() => shuffleAnswers(questions), [])
+
+    const shuffleAnswers = (questions) => {
+        questions.map(question => {
+            return {
+                question: question,
+                answers: question.answers.sort(() => Math.random() - 0.5)
+            }
+        })    
+    }
+
+    const initialState = {
+        questions: questions,
+        questionNum: 0,
+        totalQuestions: questions.length,
+        userAnswers: {}
+    }
+
+    const [state, setState] = useState(initialState);
 
     return(
-        <GlobalContext.Provider value={obj}>
+        <GlobalContext.Provider value={state}>
             {props.children}
         </GlobalContext.Provider>
     )
